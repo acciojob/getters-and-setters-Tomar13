7da6c535f3328bdf11ac389a289b0406027b937d
Tomar13 <116901663+Tomar13@users.noreply.github.com>
2023-03-28 11:18:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,67 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  set name(name) {
+    this._name = name;
+  }
+  
+  get age() {
+    return this._age;
+  }
+  
+  set age(age) {
+    this._age = age;
+  }
+  
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+}
 
-class Student extends Person {}
+class Student extends Person {
+  constructor(name, age, grade) {
+    super(name, age);
+    this.grade = grade;
+  }
+  
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
+  
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I am a student in grade ${this.grade}.`);
+  }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+  constructor(name, age, subject) {
+    super(name, age);
+    this.subject = subject;
+  }
+  
+  teach() {
+    console.log(`${this.name} is teaching ${this.subject}.`);
+  }
+  
+  sayHello() {
+    console.log(`Hello, my name is ${this.name} and I teach ${this.subject}.`);
+  }
+}
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+const person = new Person('Alice', 25);
+person.sayHello();
+
+const student = new Student('Bob', 20, 10);
+student.sayHello();
+student.study();
+
+const teacher = new Teacher('Charlie', 35, 'Math');
+teacher.sayHello();
+teacher.teach();
